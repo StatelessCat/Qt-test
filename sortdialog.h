9d11560f572d3b5f756d5d8ac6df46e0c1c12708
SchrodingerCat <rcleveque@gmail.com>
2013-07-02 21:40:25 +0200

    remove memory leak on main, fix wrong inherance order on sortdialog.h

--- sortdialog.h
+++ sortdialog.h
@@ -5,15 +5,12 @@
 
 #include "ui_sortdialog.h"
 
-class SortDialog : public Ui::SortDialog, public QDialog
+class SortDialog : public QDialog, public Ui::SortDialog
 {
-    //Q_OBJECT
+    Q_OBJECT
 public:
     explicit SortDialog(QWidget * parent = 0);
     void setColumnRange(QChar first, QChar last);
-signals:
-    
-public slots:
     
 };
 
